22b28cacb006e44920dab654cb3fb6a5d86dabeb
Joshua Book <joshua.book@gmail.com>
2016-11-03 09:15:54 -0400

    handle JWT auth token encoding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_authentication_token
+
   def index; end
+
+  private
+
+  def set_authentication_token
+    payload = {current_user_email: current_user.email}
+    @token = JWT.encode payload, 'new-secret', 'HS256'
+  end
 end
